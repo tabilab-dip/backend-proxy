c0324d20d5f8daccb2e4b940284f78971ae985f2
selim-karaduman <selim.karaduman07@gmail.com>
2021-04-03 03:44:16 +0300

    design of the system

--- backend_proxy/api/endpoints.py
+++ backend_proxy/api/endpoints.py
@@ -15,18 +15,6 @@ def add_tool():
         object_dict["root_json"] = root_json
         object_dict["form_data_json"] = form_data_json
         tool_schema = ToolSchema().load(object_dict)
-        """
-        TODO:
-        1- Read src
-        2- Check if dict's enum is unique (of tool_schema)
-            2.1- Implement db Class
-            2.2- Implement Tool Services which uses the db Class; Check uniqueness
-        3- If all fine return 200
-        4- Final test
-            - Incrementally add folders/files to morp_sak
-            - Try with frontend forms (at each increment too)
-            - 
-        """
         logs = ""
         response = dict({"title": "Tool is added to the proxy",
                          "subTitle": "Logs: {}".format(logs)})
@@ -34,6 +22,38 @@ def add_tool():
         response = dict({"title": "500; Server Error",
                          "subTitle": "Logs: {}".format(str(e))})
     return json.dumps(response)
+
+
+"""
+list endpoints:
+Step 1 - Dynamic System:
+1- POST: add_tool:
+    given a tool description, add it to the database; the tool's enum must be unique
+2- GET: list all tools with all of their information
+    return all of the tools with all of their information
+3- UPDATE: update_tool:
+    given a tool description, find the one with the same enum, and update it
+        - this does not update the enum, since it is unique, returns error if enum does not exist
+        - hence, frontend should not let user try to edit it in the first place
+4- DELETE: delete_tool:
+    given a tool description, delete it: only enum is enough
+5- GET: get_tool_names:
+    return names and enums of all of the tools in the db, don't return any other info,
+    no input requried here, just return 
+6- GET: get_tool_ui_info: 
+    given an enum, return the author_json, root_json, form_data_json fields
+7- POST: run_tool:
+    given enum of a tool
+    retrieves the tool's info, make HTTP request to that IP:PORT,
+    get back the info, and depending on brat format do further 
+        parsing in the parsing to get standoff format
+
+
+Step 2 - Authentication:
+1- Login
+    - required endpoints for authentication, and update the existing endpoints to check for that.
+
+"""
 
 
 if __name__ == '__main__':
