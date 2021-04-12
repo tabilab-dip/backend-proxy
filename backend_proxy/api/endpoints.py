bb1b625d03b6e64a9a8a90466c2581d4d4810c7a
selim-karaduman <selim.karaduman07@gmail.com>
2021-04-12 21:03:42 +0300

    define endpoints

--- backend_proxy/api/endpoints.py
+++ backend_proxy/api/endpoints.py
@@ -105,45 +105,32 @@ def run_tool(enum):
         status = 500
     return get_response_json(data, status)
 
+# =============================================================
+
+
+@app.route("/api/user/login", methods=["POST"])
+def login_user():
+    pass
+
+
+@app.route("/api/user/logout", methods=["GET"])
+def logout_user():
+    pass
+
+
+@app.route("/api/user/register", methods=["POST"])
+def register_user():
+    pass
+
+
+@app.route("/api/user/update", methods=["POST"])
+def update_user():
+    pass
+
 
 def get_response_json(data, status):
     return (json.dumps(data), status, {'content-type': 'application/json'})
 
 
-"""
-list endpoints:
-Step 0 - Move logic etc to service
-Step 1 - Dynamic System:
-1- POST: add_tool:
-    given a tool description, add it to the database; the tool's enum must be unique
-2- GET: list_all_tools 
-    list all with all of their information
-    return all of the tools with all of their information
-3- UPDATE: update_tool:
-    given a tool description, find the one with the same enum, and update it
-        - this does not update the enum, since it is unique, returns error if enum does not exist
-        - hence, frontend should not let user try to edit it in the first place
-4- DELETE: delete_tool:
-    given a tool description, delete it: only enum is enough
-5- GET: get_tool_names:
-    return names and enums of all of the tools in the db, don't return any other info,
-    no input requried here, just return 
-6- GET: get_tool_ui_info: 
-    given an enum, return the author_json, root_json, form_data_json fields
-7- POST: run_tool:
-    given enum of a tool
-    retrieves the tool's info, make HTTP request to that IP:PORT,
-    get back the info, and depending on brat format do further 
-        parsing in the parsing to get standoff format
-
-
-Step 2 - Authentication:
-1- Login
-    - required endpoints for authentication, and update the existing endpoints to check for that.
-    - update password etc.
-    - timely session
-    - update DB class to load initial passwd from OS environ variable
-
-"""
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
